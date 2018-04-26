95e584bb5e9bc1ede60f2956a6050841b8c96117
James Harris <james.harris@icecave.com.au>
2018-04-27 08:59:27 +1000

    Add `Envelope` struct.

---
+++ src/ax/envelope.go
@@ -0,0 +1,87 @@
+package ax
+
+import "time"
+
+// Envelope is a container for a message and its associated meta-data.
+type Envelope struct {
+	// MessageID is a globally unique identifier for a single message.
+	//
+	// Among other things, the message ID is often used during message
+	// de-duplicatation in order to provide exactly-one handling semantics.
+	MessageID MessageID
+
+	// CausationID is the ID of the message that directly caused this message to
+	// occur.
+	//
+	// Messages can be thought of as occurring within a tree of messages. The
+	// CausationID identifies the direct parent message within that tree.
+	//
+	// When a message is injected into the messaging system via a MessageBus,
+	// the CausationID is set to the MessageID, that is, the message is its own
+	// cause.
+	//
+	// When a message is sent via a MessageContext, the CausationID is
+	// automatically set to the MessageID of the message being handled in that
+	// context.
+	CausationID MessageID
+
+	// CorrelationID is the ID of the message that (perhaps indirectly) caused
+	// this message to occur.
+	//
+	// Messages can be thought of as occurring within a tree of messages. The
+	// CorrelationID identifies the message at the root of that tree.
+	//
+	// When a message is injected into the message system via a MessageBus,
+	// the CorrelationID is set to the MessageID, that is, the message is at the
+	// root of the tree.
+	//
+	// When a message is sent via a MessageContext, the CorrelationID is
+	// automatically set to the CorrelationID of the message being handled in
+	// that context.
+	CorrelationID MessageID
+
+	// Time is the time at which the message was created.
+	//
+	// Time is populated via the regular Go system clock, and as such there are
+	// almost no guarantees about the accuracy of the time. It must not be
+	// assumed that messages will arrive or even be delivered in any
+	// chronological order.
+	Time time.Time
+
+	// Message is the application-defined message encapsulated by the envelope.
+	Message Message
+}
+
+// NewEnvelope creates a new message envelope containing m.
+//
+// It generates a UUID-based message message ID and configures the envelope such
+// that m is at the root of a new tree of messages.
+func NewEnvelope(m Message) Envelope {
+	env := Envelope{
+		Time:    time.Now(),
+		Message: m,
+	}
+
+	env.MessageID.GenerateUUID()
+	env.CausationID = env.MessageID
+	env.CorrelationID = env.MessageID
+
+	return env
+}
+
+// NewChild returns a new message envelope containing m.
+//
+// It generates a UUID-based message ID and configures the envelope such that
+// m is a child of e.Message within an existing tree of messages.
+func (e Envelope) NewChild(m Message) Envelope {
+	env := Envelope{
+		CorrelationID: e.CorrelationID,
+		CausationID:   e.MessageID,
+		Time:          time.Now(),
+		Message:       m,
+	}
+
+	env.MessageID.GenerateUUID()
+
+	return env
+}
---
+++ src/ax/envelope_test.go
@@ -0,0 +1,61 @@
+package ax_test
+
+import (
+	. "github.com/jmalloc/ax/src/ax"
+	"github.com/jmalloc/ax/src/ax/internal/messagetest"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	uuid "github.com/satori/go.uuid"
+)
+
+var _ = Describe("Envelope", func() {
+	Describe("NewEnvelope", func() {
+		message := &messagetest.Message{}
+		env := NewEnvelope(message)
+
+		It("returns an envelope containing the message", func() {
+			Expect(env.Message).To(Equal(message))
+		})
+
+		It("generates a UUID message ID", func() {
+			u, err := uuid.FromString(env.MessageID.Get())
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(u.String()).To(Equal(env.MessageID.Get()))
+		})
+
+		It("sets the causation ID to the message ID", func() {
+			Expect(env.CausationID).To(Equal(env.MessageID))
+		})
+
+		It("sets the correlation ID to the message ID", func() {
+			Expect(env.CorrelationID).To(Equal(env.MessageID))
+		})
+	})
+
+	Describe("NewChild", func() {
+		rootMessage := &messagetest.Message{}
+		root := NewEnvelope(rootMessage)
+		branchMessage := &messagetest.Message{}
+		branch := root.NewChild(branchMessage)
+		leafMessage := &messagetest.Message{}
+		leaf := branch.NewChild(leafMessage)
+
+		It("returns an envelope containing the message", func() {
+			Expect(leaf.Message).To(Equal(leafMessage))
+		})
+
+		It("generates a UUID message ID", func() {
+			u, err := uuid.FromString(leaf.MessageID.Get())
+			Expect(err).ShouldNot(HaveOccurred())
+			Expect(u.String()).To(Equal(leaf.MessageID.Get()))
+		})
+
+		It("sets the causation ID to the parent's message ID", func() {
+			Expect(leaf.CausationID).To(Equal(branch.MessageID))
+		})
+
+		It("sets the correlation ID to the root's message ID", func() {
+			Expect(leaf.CorrelationID).To(Equal(root.MessageID))
+		})
+	})
+})
