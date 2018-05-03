68593634e01ea61c1d3f8ac8aabec5e5e5d179d2
James Harris <james.harris@icecave.com.au>
2018-05-04 07:51:57 +1000

    Rename `FormatID()` to `Format()`.

--- src/ax/ident/format_test.go
+++ src/ax/ident/format_test.go
@@ -9,14 +9,14 @@ import (
 var _ = Describe("Format", func() {
 	Context("when the identifier is formatted like a UUID", func() {
 		It("returns the portion up to the first hyphen", func() {
-			s := FormatID("7fd0fd54-28bc-49f2-8916-8128d8cfc69e")
+			s := Format("7fd0fd54-28bc-49f2-8916-8128d8cfc69e")
 			Expect(s).To(Equal("7fd0fd54"))
 		})
 	})
 
 	Context("when the identifier is not formatted like a UUID", func() {
 		It("returns the entire identifier", func() {
-			s := FormatID("<identifier>")
+			s := Format("<identifier>")
 			Expect(s).To(Equal("<identifier>"))
 		})
 	})
