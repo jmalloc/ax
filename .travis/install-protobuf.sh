be7fc39cb54870593eaf57efb934a4c72fb0ab9a
Kevin Millar <koden.km@gmail.com>
2018-05-08 16:22:40 +1000

    Fix path of proto files in include.

--- .travis/install-protobuf.sh
+++ .travis/install-protobuf.sh
@@ -5,7 +5,7 @@ if [ ! -d "$HOME/protobuf/lib" ]; then
   wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
   unzip protoc-3.5.1-linux-x86_64.zip
   sudo cp bin/protoc /usr/local/bin/
-  sudo cp include/* /usr/local/include/
+  sudo cp -R include/google/protobuf/* /usr/local/include/
 else
   echo "Using cached directory."
 fi
