9a12dd880b2f58ffc0041e14d54a4684a57a9938
Kevin Millar <koden.km@gmail.com>
2018-05-08 15:15:54 +1000

    Update protobuf installer.

--- .travis/install-protobuf.sh
+++ .travis/install-protobuf.sh
@@ -2,9 +2,9 @@
 set -e
 # check to see if protobuf folder is empty
 if [ ! -d "$HOME/protobuf/lib" ]; then
-  wget https://protobuf.googlecode.com/files/protobuf-2.4.1.tar.gz
-  tar -xzvf protobuf-2.4.1.tar.gz
-  cd protobuf-2.4.1 && ./configure --prefix=$HOME/protobuf && make && make install
+  wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
+  unzip protoc-3.5.1-linux-x86_64.zip
+  cd protoc-3.5.1-linux-x86_64 && ./configure --prefix=$HOME/protobuf && make && make install
 else
   echo "Using cached directory."
 fi
