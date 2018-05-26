9270b342bed618cf7c1df9b099771b3589ccc7fa
James Harris <james.harris@icecave.com.au>
2018-05-26 12:35:26 +1000

    Add a saga behaviour for mapping by instance ID.

--- src/ax/saga/behavior.go
+++ src/ax/saga/behavior.go
@@ -7,8 +7,8 @@ import (
 	"github.com/jmalloc/ax/src/ax"
 )
 
-// IgnoreNotFound is an embeddable struct that implements a Saga.HandleNotFound
-// method that is a no-op.
+// IgnoreNotFound is an embeddable struct that implements a
+// Saga.HandleNotFound() method that is a no-op.
 type IgnoreNotFound struct{}
 
 // HandleNotFound always returns nil.
@@ -16,11 +16,23 @@ func (IgnoreNotFound) HandleNotFound(context.Context, ax.Sender, ax.Envelope) er
 	return nil
 }
 
-// ErrorIfNotFound is an embeddable struct that implements a Saga.HandleNotFound
-// method that always returns an error.
+// ErrorIfNotFound is an embeddable struct that implements a
+// Saga.HandleNotFound() method that always returns an error.
 type ErrorIfNotFound struct{}
 
 // HandleNotFound always returns an error.
 func (ErrorIfNotFound) HandleNotFound(_ context.Context, _ ax.Sender, _ ax.Envelope) error {
 	return errors.New("could not find a saga instance to handle message")
+}
+
+// MapByInstanceID is an embeddable struct that implements a
+// Saga.MappingKeysForInstance() method that always returns a key set containing
+// only the instance ID.
+type MapByInstanceID struct{}
+
+// MappingKeysForInstance always returns a key set containing only the instance ID.
+func (MapByInstanceID) MappingKeysForInstance(_ context.Context, i Instance) (KeySet, error) {
+	return NewKeySet(
+		i.InstanceID.Get(),
+	), nil
 }
