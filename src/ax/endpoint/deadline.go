e3abf97d8c7de1fb823ab5827acb9d4f43db49f4
Kevin Millar <koden.km@gmail.com>
2018-05-28 13:03:21 +1000

    Add import and correct use of duration

--- src/ax/endpoint/deadline.go
+++ src/ax/endpoint/deadline.go
@@ -2,34 +2,35 @@ package endpoint
 
 import (
 	"context"
+	"time"
 )
 
 // Deadline is an inbound pipeline that sets a context deadline before
 // forwarding on to the next stage.
 type Deadline struct {
-	Deadline time.Time
+	Deadline time.Duration
 	Cancel   context.CancelFunc
-	Next     endpoint.InboundPipeline
+	Next     InboundPipeline
 }
 
-const DefaultDeadlineMs = 1000
+const DefaultDeadline = 1000 * time.Millisecond
 
 // Initialize is called during initialization of the endpoint, after the
 // transport is initialized. It can be used to inspect or further configure the
 // endpoint as per the needs of the pipeline.
 func (d Deadline) Initialize(ctx context.Context, ep *Endpoint) error {
-	if d.Deadline.IsZero() {
-		d.Deadline = DefaultDeadlineMs * time.Millisecond
+	if d.Deadline == 0 {
+		d.Deadline = DefaultDeadline
 	}
-	return i.Next.Initialize(ctx, ep)
+	return d.Next.Initialize(ctx, ep)
 }
 
 // Accept forwards an inbound message through the pipeline until
 // it is handled by some application-defined message handler(s).
 func (d Deadline) Accept(ctx context.Context, sink MessageSink, env InboundEnvelope) error {
-	ctx, d.Cancel = context.WithDeadline(ctx, d.Deadline)
+	ctx, d.Cancel = context.WithTimeout(ctx, d.Deadline)
 	go cancelDeadline(ctx, d)
-	return i.Next.Accept(ctx, sink, env)
+	return d.Next.Accept(ctx, sink, env)
 }
 
 func cancelDeadline(ctx context.Context, d Deadline) {
