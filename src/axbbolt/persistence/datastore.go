e2195c684f2df78460b2888a3cd1141c262c74e3
Danil Petrov <danil@codeworx.com.au>
2018-06-14 00:38:33 +1000

    initial bolt datastore and transaction implementation

---
+++ src/axbbolt/persistence/datastore.go
@@ -0,0 +1,30 @@
+package persistence
+
+import (
+	"context"
+
+	bolt "github.com/coreos/bbolt"
+	"github.com/jmalloc/ax/src/ax/persistence"
+)
+
+// DataStore is a MySQL-backed implementation of Ax's persistence.DataStore
+// interface.
+type DataStore struct {
+	DB *bolt.DB
+}
+
+// BeginTx starts a new transaction.
+func (ds *DataStore) BeginTx(ctx context.Context) (persistence.Tx, persistence.Committer, error) {
+	tx, err := ds.DB.Begin(true)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &Tx{ds, tx}, tx, nil
+}
+
+// ExtractDB returns the Bolt database within ds.
+// It panics if ds is not a *DataStore.
+func ExtractDB(ds persistence.DataStore) *bolt.DB {
+	return ds.(*DataStore).DB
+}
