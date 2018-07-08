2793c05f22f1ca204afb5ff68be4f037a5983361
James Harris <james.harris@icecave.com.au>
2018-07-09 06:33:10 +1000

    Move `SelfValidatingMessage` into `validator.go`.

--- src/ax/endpoint/validator.go
+++ src/ax/endpoint/validator.go
@@ -6,6 +6,19 @@ import (
 	"github.com/jmalloc/ax/src/ax"
 )
 
+// SelfValidatingMessage is a message that can perform its own superficial
+// validation.
+type SelfValidatingMessage interface {
+	ax.Message
+
+	// Validate returns a non-nil error if the message is invalid. It is up to
+	// message implementation to check validity criteria.
+	//
+	// This method is invoked by SelfValidator that is one the default
+	// validators to verify the message.
+	Validate() error
+}
+
 // Validator is an interface for validating messages.
 //
 // Application-defined validators can be implemented to provide superficial and
