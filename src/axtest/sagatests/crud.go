e8f84405b5380426cfaef23a93781858f920ff31
Danil Petrov <danil@codeworx.com.au>
2018-07-12 21:02:12 +1000

    wip - saga crud test pkg

--- src/axtest/sagatests/crud.go
+++ src/axtest/sagatests/crud.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"time"
 
+	"github.com/golang/protobuf/proto"
+
 	"github.com/jmalloc/ax/src/ax/saga"
 	"github.com/jmalloc/ax/src/ax/saga/persistence/crud"
 	"github.com/jmalloc/ax/src/axtest/testmessages"
@@ -107,178 +109,150 @@ func CRUDRepositorySuite(
 					)
 					m.Expect(err).ShouldNot(m.HaveOccurred())
 					m.Expect(i.InstanceID).Should(m.Equal(expected.InstanceID))
+					m.Expect(i.Revision).Should(m.Equal(expected.Revision))
+					m.Expect(proto.Equal(i.Data, expected.Data)).Should(m.BeTrue())
 				})
-
-				// g.It("does not return messages that are marked as sent", func() {
-				// 	tx, com, err := store.BeginTx(ctx)
-				// 	m.Expect(err).ShouldNot(m.HaveOccurred())
-				// 	defer com.Rollback()
-
-				// 	err = repo.MarkAsSent(
-				// 		ctx,
-				// 		tx,
-				// 		m1,
-				// 	)
-				// 	m.Expect(err).ShouldNot(m.HaveOccurred())
-
-				// 	err = com.Commit()
-				// 	m.Expect(err).ShouldNot(m.HaveOccurred())
-
-				// 	envs, _, err := repo.LoadOutbox(ctx, store, causationID)
-				// 	m.Expect(err).ShouldNot(m.HaveOccurred())
-				// 	m.Expect(
-				// 		axtest.ConsistsOfOutboundEnvelopes(envs, m2),
-				// 	).To(m.BeTrue())
-				// })
 			})
 
-			// g.Context("when the outbox exists but contains no messages", func() {
-			// 	g.BeforeEach(func() {
-			// 		tx, com, err := store.BeginTx(ctx)
-			// 		if err != nil {
-			// 			panic(err)
-			// 		}
-			// 		defer com.Rollback()
+			g.Context("when the instance does not exists", func() {
+				var (
+					pk  string
+					tx  persistence.Tx
+					com persistence.Committer
+				)
+				g.BeforeEach(func() {
+					var err error
+					pk = "<test>"
+					tx, com, err = store.BeginTx(ctx)
+					if err != nil {
+						panic(err)
+					}
+				})
+				g.AfterEach(func() {
+					com.Rollback()
+				})
 
-			// 		err = repo.SaveOutbox(
-			// 			ctx,
-			// 			tx,
-			// 			causationID,
-			// 			nil,
-			// 		)
-			// 		if err != nil {
-			// 			panic(err)
-			// 		}
+				g.It("returns false", func() {
+					id := saga.GenerateInstanceID()
+					_, ok, err := repo.LoadSagaInstance(
+						ctx,
+						tx,
+						pk,
+						id,
+					)
+					m.Expect(err).ShouldNot(m.HaveOccurred())
+					m.Expect(ok).To(m.BeFalse())
+				})
+			})
 
-			// 		err = com.Commit()
-			// 		if err != nil {
-			// 			panic(err)
-			// 		}
-			// 	})
+			g.Context("instance is found, but belongs to a different saga", func() {
+				var (
+					expected saga.Instance
+					pk       string
+					tx       persistence.Tx
+					com      persistence.Committer
+				)
+				g.BeforeEach(func() {
+					var err error
+					pk = "<test>"
+					expected = saga.Instance{
+						InstanceID: saga.GenerateInstanceID(),
+						Revision:   saga.Revision(0),
+						Data: &testmessages.Data{
+							Value: "<foo>",
+						},
+					}
 
-			// 	g.It("returns true", func() {
-			// 		_, ok, err := repo.LoadOutbox(ctx, store, causationID)
-			// 		m.Expect(err).ShouldNot(m.HaveOccurred())
-			// 		m.Expect(ok).To(m.BeTrue())
-			// 	})
-			// })
+					tx, com, err = store.BeginTx(ctx)
+					if err != nil {
+						panic(err)
+					}
 
-			// g.Context("when the outbox does not exist", func() {
-			// 	g.It("returns false", func() {
-			// 		_, ok, err := repo.LoadOutbox(ctx, store, causationID)
-			// 		m.Expect(err).ShouldNot(m.HaveOccurred())
-			// 		m.Expect(ok).To(m.BeFalse())
-			// 	})
-			// })
+					err = repo.SaveSagaInstance(
+						ctx,
+						tx,
+						pk,
+						expected,
+					)
+					if err != nil {
+						panic(err)
+					}
+
+					err = com.Commit()
+					if err != nil {
+						panic(err)
+					}
+
+					tx, com, err = store.BeginTx(ctx)
+					if err != nil {
+						panic(err)
+					}
+				})
+				g.AfterEach(func() {
+					com.Rollback()
+				})
+
+				g.It("returns an error", func() {
+					_, _, err := repo.LoadSagaInstance(
+						ctx,
+						tx,
+						"<unknown>",
+						expected.InstanceID,
+					)
+					m.Expect(err).Should(m.HaveOccurred())
+				})
+			})
 		})
 
-		// g.Describe("SaveOutbox", func() {
-		// 	g.It("returns an error if the outbox already exists", func() {
-		// 		tx, com, err := store.BeginTx(ctx)
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
-		// 		defer com.Rollback()
+		g.Describe("SaveSagaInstance", func() {
+			g.Context("when the instance doesn't exist (insert)", func() {
+				var (
+					pk  string
+					tx  persistence.Tx
+					com persistence.Committer
+				)
+				g.BeforeEach(func() {
+					var err error
+					pk = "<test>"
+					tx, com, err = store.BeginTx(ctx)
+					if err != nil {
+						panic(err)
+					}
+				})
 
-		// 		err = repo.SaveOutbox(
-		// 			ctx,
-		// 			tx,
-		// 			causationID,
-		// 			nil,
-		// 		)
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
+				g.AfterEach(func() {
+					com.Rollback()
+				})
 
-		// 		err = repo.SaveOutbox(
-		// 			ctx,
-		// 			tx,
-		// 			causationID,
-		// 			nil,
-		// 		)
-		// 		m.Expect(err).Should(m.HaveOccurred())
-		// 	})
+				g.It("returns nil", func() {
+					i := saga.Instance{
+						InstanceID: saga.GenerateInstanceID(),
+						Revision:   saga.Revision(0),
+						Data: &testmessages.Data{
+							Value: "<foo>",
+						},
+					}
+					err := repo.SaveSagaInstance(
+						ctx,
+						tx,
+						pk,
+						i,
+					)
+					m.Expect(err).ShouldNot(m.HaveOccurred())
+				})
+			})
 
-		// 	g.It("does not add new messages to an existing outbox", func() {
-		// 		tx, com, err := store.BeginTx(ctx)
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
-		// 		defer com.Rollback()
+			g.Context("when the instance exists (update)", func() {
 
-		// 		err = repo.SaveOutbox(
-		// 			ctx,
-		// 			tx,
-		// 			causationID,
-		// 			nil,
-		// 		)
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
+			})
 
-		// 		env := endpoint.OutboundEnvelope{
-		// 			Envelope: ax.Envelope{
-		// 				MessageID:     ax.GenerateMessageID(),
-		// 				CausationID:   causationID,
-		// 				CorrelationID: correlationID,
-		// 				CreatedAt:     time.Now(),
-		// 				SendAt:        time.Now(),
-		// 				Message:       &testmessages.Message{},
-		// 			},
-		// 			Operation:           endpoint.OpSendUnicast,
-		// 			DestinationEndpoint: "<dest>",
-		// 		}
+			g.Context("when the revision is not current for the existing saga instance", func() {
 
-		// 		err = repo.SaveOutbox(
-		// 			ctx,
-		// 			tx,
-		// 			causationID,
-		// 			[]endpoint.OutboundEnvelope{env},
-		// 		)
-		// 		m.Expect(err).Should(m.HaveOccurred())
+			})
 
-		// 		err = com.Commit()
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
+			g.Context("when the instance exists, but belongs to a different saga", func() {
 
-		// 		envs, ok, err := repo.LoadOutbox(ctx, store, causationID)
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
-		// 		m.Expect(envs).To(m.BeEmpty())
-		// 		m.Expect(ok).To(m.BeTrue())
-		// 	})
-		// })
-
-		// g.Describe("MarkAsSent", func() {
-		// 	g.It("does not return an error if the message has already been marked as sent", func() {
-		// 		tx, com, err := store.BeginTx(ctx)
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
-		// 		defer com.Rollback()
-
-		// 		env := endpoint.OutboundEnvelope{
-		// 			Envelope: ax.Envelope{
-		// 				MessageID:     ax.GenerateMessageID(),
-		// 				CausationID:   causationID,
-		// 				CorrelationID: correlationID,
-		// 				CreatedAt:     time.Now(),
-		// 				SendAt:        time.Now(),
-		// 				Message:       &testmessages.Message{},
-		// 			},
-		// 			Operation:           endpoint.OpSendUnicast,
-		// 			DestinationEndpoint: "<dest>",
-		// 		}
-
-		// 		err = repo.SaveOutbox(
-		// 			ctx,
-		// 			tx,
-		// 			causationID,
-		// 			[]endpoint.OutboundEnvelope{env},
-		// 		)
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
-
-		// 		err = com.Commit()
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
-
-		// 		tx, com, err = store.BeginTx(ctx)
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
-		// 		defer com.Rollback()
-
-		// 		err = repo.MarkAsSent(ctx, tx, env)
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
-
-		// 		err = repo.MarkAsSent(ctx, tx, env)
-		// 		m.Expect(err).ShouldNot(m.HaveOccurred())
-		// 	})
-		// })
+			})
+		})
 	}
 }
