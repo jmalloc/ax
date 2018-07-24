013b620555b61b35312698912ddc3a593881cb82
Danil Petrov <danil@codeworx.com.au>
2018-07-24 22:30:44 +1000

    spelling error fixed

--- src/ax/projection/offsetstore.go
+++ src/ax/projection/offsetstore.go
@@ -22,7 +22,7 @@ type OffsetStore interface {
 	// IncrementOffset increments the offset at which a consumer should resume
 	// reading from the stream by one.
 	//
-	// pk is the projector's persitence key. c is the offset that is currently
+	// pk is the projector's persistence key. c is the offset that is currently
 	// stored, as returned by LoadOffset(). If c is not the offset that is
 	// currently stored, the increment fails and a non-nil error is returned.
 	IncrementOffset(
