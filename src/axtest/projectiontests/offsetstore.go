d234dd0f6fec4f0e0a0e44fcf6075ba8e4afdadb
Danil Petrov <danil@codeworx.com.au>
2018-07-26 00:01:40 +1000

    finished projection offsetsore integration tests

--- src/axtest/projectiontests/offsetstore.go
+++ src/axtest/projectiontests/offsetstore.go
@@ -18,7 +18,7 @@ func InsertOffset(
 	repo projection.OffsetStore,
 	pk string,
 	c uint64,
-) {
+) uint64 {
 	var (
 		err error
 		tx  persistence.Tx
@@ -30,10 +30,15 @@ func InsertOffset(
 	}
 	defer com.Rollback()
 
-	repo.IncrementOffset(ctx, tx, pk, c)
-	if err != nil {
+	if repo.IncrementOffset(ctx, tx, pk, c); err != nil {
 		panic(err)
 	}
+
+	if err = com.Commit(); err != nil {
+		panic(err)
+	}
+	c++
+	return c
 }
 
 // OffsetStoreSuite returns a test suite for implementations of
@@ -66,24 +71,84 @@ func OffsetStoreSuite(
 		})
 
 		g.Describe("LoadOffset", func() {
-			g.Context("when offset exists", func() {
-				var initialOffset uint64
+			g.Context("when previous offset exists", func() {
+				var prevOffset uint64
 				g.BeforeEach(func() {
-					InsertOffset(ctx, store, repo, pk, initialOffset)
+					prevOffset = InsertOffset(ctx, store, repo, pk, 0)
 				})
 				g.It("returns no errors", func() {
-					_, err := repo.LoadOffset(ctx, store, pk)
+					_, com, err := store.BeginTx(ctx)
+					m.Expect(err).ShouldNot(m.HaveOccurred())
+					defer com.Rollback()
+
+					_, err = repo.LoadOffset(ctx, store, pk)
 					m.Expect(err).ShouldNot(m.HaveOccurred())
 				})
-				// g.It("returns the offset", func() {
-				// 	offset, _ := repo.LoadOffset(ctx, store, pk)
-				// 	m.Expect(offset).Should(m.BeNumerically("==", initialOffset+1))
-				// })
+				g.It("returns incremented offset", func() {
+					_, com, err := store.BeginTx(ctx)
+					m.Expect(err).ShouldNot(m.HaveOccurred())
+					defer com.Rollback()
+
+					offset, _ := repo.LoadOffset(ctx, store, pk)
+					m.Expect(offset).Should(m.BeNumerically("==", prevOffset))
+				})
+			})
+			g.Context("when previous offset does not exist", func() {
+				g.It("returns no errors", func() {
+					_, com, err := store.BeginTx(ctx)
+					m.Expect(err).ShouldNot(m.HaveOccurred())
+					defer com.Rollback()
+
+					_, err = repo.LoadOffset(ctx, store, pk)
+					m.Expect(err).ShouldNot(m.HaveOccurred())
+				})
+				g.It("returns zero offset", func() {
+					_, com, err := store.BeginTx(ctx)
+					m.Expect(err).ShouldNot(m.HaveOccurred())
+					defer com.Rollback()
+
+					offset, _ := repo.LoadOffset(ctx, store, pk)
+					m.Expect(offset).Should(m.BeNumerically("==", 0))
+				})
 			})
 		})
 
 		g.Describe("IncrementOffset", func() {
+			g.Context("when previous offset exists", func() {
+				var prevOffset uint64
+				g.BeforeEach(func() {
+					prevOffset = InsertOffset(ctx, store, repo, pk, 0)
+				})
+				g.It("returns no errors", func() {
+					tx, com, err := store.BeginTx(ctx)
+					m.Expect(err).ShouldNot(m.HaveOccurred())
+					defer com.Rollback()
 
+					err = repo.IncrementOffset(ctx, tx, pk, prevOffset)
+					m.Expect(err).ShouldNot(m.HaveOccurred())
+				})
+				g.Context("when the value passed does not equal to offset stored", func() {
+					g.It("returns an error", func() {
+						tx, com, err := store.BeginTx(ctx)
+						m.Expect(err).ShouldNot(m.HaveOccurred())
+						defer com.Rollback()
+
+						var offset uint64 = 3
+						err = repo.IncrementOffset(ctx, tx, pk, offset)
+						m.Expect(err).Should(m.HaveOccurred())
+					})
+				})
+			})
+			g.Context("when previous offset does not exist", func() {
+				g.It("returns no errors", func() {
+					tx, com, err := store.BeginTx(ctx)
+					m.Expect(err).ShouldNot(m.HaveOccurred())
+					defer com.Rollback()
+
+					err = repo.IncrementOffset(ctx, tx, pk, 0)
+					m.Expect(err).ShouldNot(m.HaveOccurred())
+				})
+			})
 		})
 	}
 }
