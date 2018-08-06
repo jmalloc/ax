c941638b5863763bbbb6a82056a5fe698a519e2a
Danil Petrov <danil@codeworx.com.au>
2018-08-06 23:49:41 +1000

    added more helpers to deal with pb messages

--- src/axbolt/internal/boltutil/protobuf.go
+++ src/axbolt/internal/boltutil/protobuf.go
@@ -25,3 +25,47 @@ func UnmarshalProto(b *bolt.Bucket, k []byte, m proto.Message) (bool, error) {
 
 	return true, proto.Unmarshal(buf, m)
 }
+
+// GetProto retrieves a protobuf-encoded message indexed with key from sequence of
+// bkts, where each proceeding bucket is a subbucket of the previous one.
+//
+// It returns false if any of bkts do no exist or if the key in the last child
+// bucket does not exist. It returns an error if one occurs in the process of
+// unmarshaling binary data into m.
+func GetProto(
+	tx *bolt.Tx,
+	key string,
+	m proto.Message,
+	bkts ...string,
+) (bool, error) {
+	pb := Get(tx, key, bkts...)
+	if pb == nil {
+		return false, nil
+	}
+	err := proto.Unmarshal(pb, m)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+// PutProto places a protobuf-encoded message indexed with key to sequence of
+// bkts, where each proceeding bucket is a subbucket of the previous one. If any
+// of bkts don't exists, this function attempts to create them using MakeBtk
+// function under the hood.
+//
+// It returns an error if bucket creation fails. It returns an error if one
+// occurs in the process of marshalling m into binary.
+func PutProto(
+	tx *bolt.Tx,
+	key string,
+	m proto.Message,
+	bkts ...string,
+) error {
+	pb, err := proto.Marshal(m)
+	if err != nil {
+		return err
+	}
+
+	return Put(tx, key, pb, bkts...)
+}
