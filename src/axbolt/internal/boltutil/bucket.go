cb6d7074bc61319d669bb55d7f76117fffdf09eb
Danil Petrov <danil@codeworx.com.au>
2018-08-06 23:48:59 +1000

    changing and reformating comments in bucket util functions

--- src/axbolt/internal/boltutil/bucket.go
+++ src/axbolt/internal/boltutil/bucket.go
@@ -17,14 +17,14 @@ const (
 	BktPathSeparator = "/"
 )
 
-// GetBkt retrieves a Bolt DB bucket with a name of the last string of ss.
-// All preceding strings in ss represent names the parent buckets of the last
-// string (bucket name). If any bucket in ss is nonexistant, nil is returned.
-func GetBkt(tx *bolt.Tx, ss ...string) *bolt.Bucket {
+// GetBkt retrieves a Bolt DB bucket with a name of the last string of bkts.
+// All preceding strings in bkts represent names the parent buckets of the last
+// string (bucket name). If any bucket in bkts is nonexistant, nil is returned.
+func GetBkt(tx *bolt.Tx, bkts ...string) *bolt.Bucket {
 	var (
 		b *bolt.Bucket
 	)
-	for _, s := range ss {
+	for _, s := range bkts {
 		if b == nil {
 			b = tx.Bucket([]byte(s))
 			if b == nil {
@@ -44,15 +44,15 @@ func GetBkt(tx *bolt.Tx, ss ...string) *bolt.Bucket {
 // with BktPathSeparator. This method retrieves the last bucket in the path. If
 // any bucket in the path is nonexistant, nil is returned.
 func GetBktWithPath(tx *bolt.Tx, path string) *bolt.Bucket {
-	ss := strings.Split(path, BktPathSeparator)
-	return GetBkt(tx, ss...)
+	bkts := strings.Split(path, BktPathSeparator)
+	return GetBkt(tx, bkts...)
 }
 
-// Get retrieves a value from the bucket which is the last in ss and it has a
-// key key. If any bucket in ss is nonexistant nil is returned. If key does not
-// exist in the last bucket of ss, nil is returned.
-func Get(tx *bolt.Tx, key string, ss ...string) []byte {
-	bkt := GetBkt(tx, ss...)
+// Get retrieves a value from the bucket which is the last in bkts and it has a
+// key key. If any bucket in bkts is nonexistant nil is returned. If key does
+// not exist in the last bucket of bkts, nil is returned.
+func Get(tx *bolt.Tx, key string, bkts ...string) []byte {
+	bkt := GetBkt(tx, bkts...)
 	if bkt == nil {
 		return nil
 	}
@@ -70,22 +70,23 @@ func GetWithPath(tx *bolt.Tx, key, path string) []byte {
 	return bkt.Get([]byte(key))
 }
 
-// MakeBkt creates Bolt DB buckets specified in ss. Each subsequent bucket in ss
-// is created as a subbucket of preceding bucket. The last created bucket of ss
-// is returned under normal conditions. If any bucket specified in ss already
-// exists no action is taken on the bucket. If any Bolt DB error occurres in the
-// process of creating the buckets, nil and the error are returned.
+// MakeBkt creates Bolt DB buckets specified in bkts. Each subsequent bucket in
+// bkts is created as a subbucket of preceding bucket. The last created bucket
+// of bkts is returned under normal conditions. If any bucket specified in bkts
+// already exists no action is taken on the bucket. If any Bolt DB error
+// occurres in the process of creating the buckets, nil and the error are
+// returned.
 //
 // Please note that this method trims any leading and trailing whitespaces
-// (using strings.TrimSpace function) from all strings passed as ss. It ignores
-// creation of a bucket for any strings in ss that result in zero length after
-// trimming.
-func MakeBkt(tx *bolt.Tx, ss ...string) (*bolt.Bucket, error) {
+// (using strings.TrimSpace function) from all strings passed as bkts. It
+// ignores creation of a bucket for any strings in bkts that result in zero
+// length after trimming.
+func MakeBkt(tx *bolt.Tx, bkts ...string) (*bolt.Bucket, error) {
 	var (
 		b   *bolt.Bucket
 		err error
 	)
-	for _, s := range ss {
+	for _, s := range bkts {
 		s = strings.TrimSpace(s)
 		if len(s) > 0 {
 			if b == nil {
@@ -106,16 +107,16 @@ func MakeBkt(tx *bolt.Tx, ss ...string) (*bolt.Bucket, error) {
 // MakeBktWithPath creates Bolt DB buckets from path separated with BktPathSeparator
 // For all usage cases please refer to function MakeBkt.
 func MakeBktWithPath(tx *bolt.Tx, path string) (*bolt.Bucket, error) {
-	ss := strings.Split(path, BktPathSeparator)
-	return MakeBkt(tx, ss...)
+	bkts := strings.Split(path, BktPathSeparator)
+	return MakeBkt(tx, bkts...)
 }
 
 // Put places a value val with key key in the bucket specified as a last string
-// of ss. Any nonexistant buckets in ss will be created before the key and the
-// value are placed. For any cases related to bucket creation of ss, please
-// refer to function MakeBkt.
-func Put(tx *bolt.Tx, key string, val []byte, ss ...string) error {
-	bkt, err := MakeBkt(tx, ss...)
+// of bkts. Any nonexistant buckets in bkts will be created before the key and
+// the value are placed. For any cases related to bucket creation of bkts,
+// please refer to function MakeBkt.
+func Put(tx *bolt.Tx, key string, val []byte, bkts ...string) error {
+	bkt, err := MakeBkt(tx, bkts...)
 	if err != nil {
 		return err
 	}
@@ -136,18 +137,18 @@ func PutWithPath(tx *bolt.Tx, key string, val []byte, path string) error {
 }
 
 // Delete deletes an entry with key key in the bucket specified as a last item
-// in the ss. If there are any nonexistant buckets in ss the function returns
-// nil.
-func Delete(tx *bolt.Tx, key string, ss ...string) error {
-	if bkt := GetBkt(tx, ss...); bkt != nil {
+// in the bkts. If there are any nonexistant buckets in bkts the function
+// returns nil.
+func Delete(tx *bolt.Tx, key string, bkts ...string) error {
+	if bkt := GetBkt(tx, bkts...); bkt != nil {
 		return bkt.Delete([]byte(key))
 	}
 	return nil
 }
 
-// DeleteWithPath deletes an entry with key key in the bucket specified as a last
-// in the separated by BktPathSeparator. If there are any nonexistant buckets in ss the function returns
-// nil.
+// DeleteWithPath deletes an entry with key key in the bucket specified as a
+// last in the separated by BktPathSeparator. If there are any nonexistant
+// buckets in the path, the function returns nil.
 func DeleteWithPath(tx *bolt.Tx, key string, path string) error {
 	if bkt := GetBktWithPath(tx, path); bkt != nil {
 		return bkt.Delete([]byte(key))
