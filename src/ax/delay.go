94cec177a9d926819478e26d8865b2ccc38fe4ea
James Harris <james.harris@icecave.com.au>
2018-11-14 09:54:12 +1000

    Compute `Delay()` option from the message creation time.

--- src/ax/delay.go
+++ src/ax/delay.go
@@ -5,23 +5,32 @@ import "time"
 // Delay is an option that delays sending the message until a duration has
 // passed. Events can not be delayed.
 func Delay(d time.Duration) ExecuteOption {
-	t := time.Now().Add(d)
-	return DelayUntil(t)
+	return delayOption{d}
 }
 
 // DelayUntil is an option that delays sending the message until a specific
 // time. Events can not be delayed.
 func DelayUntil(t time.Time) ExecuteOption {
-	return delayOption{t}
+	return delayUntilOption{t}
 }
 
-// delayOption provides the implementation of SendOption for the Delay and
-// DelayUntil options.
+// delayOption provides the implementation of SendOption for the Delay option.
 type delayOption struct {
-	Time time.Time
+	Delay time.Duration
 }
 
 func (o delayOption) ApplyExecuteOption(env *Envelope) error {
+	env.SendAt = env.CreatedAt.Add(o.Delay)
+	return nil
+}
+
+// delayUntilOption provides the implementation of SendOption for the DelayUntil
+// options.
+type delayUntilOption struct {
+	Time time.Time
+}
+
+func (o delayUntilOption) ApplyExecuteOption(env *Envelope) error {
 	env.SendAt = o.Time
 	return nil
 }
