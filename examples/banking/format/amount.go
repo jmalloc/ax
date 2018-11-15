625d188dadde52f859470480147bfa1cca4c3e9d
James Harris <james.harris@icecave.com.au>
2018-11-16 09:45:52 +1000

    Mark transfer aggregate as complete.

--- examples/banking/format/amount.go
+++ examples/banking/format/amount.go
@@ -4,6 +4,12 @@ import "fmt"
 
 // Amount formats a cent amount as dollars.
 func Amount(v int32) string {
+	f := "-$%d.%02d"
+	if v < 0 {
+		v = -v
+		f = "-" + f
+	}
+
 	return fmt.Sprintf(
 		"$%d.%02d",
 		v/100,
