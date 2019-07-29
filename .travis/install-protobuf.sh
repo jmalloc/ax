9d5bb10d54c10ba1ca94237b86c137be082417e7
James Harris <mailjamesharris@gmail.com>
2019-07-30 08:16:29 +1000

    Try to fix protoc install.

--- .travis/install-protobuf.sh
+++ .travis/install-protobuf.sh
@@ -4,7 +4,9 @@ set -e
 if [ ! -d "$HOME/protobuf/lib" ]; then
   wget https://github.com/google/protobuf/releases/download/v3.5.1/protoc-3.5.1-linux-x86_64.zip
   unzip protoc-3.5.1-linux-x86_64.zip
+  chmod +x bin/protoc
   sudo cp bin/protoc /usr/local/bin/
+  ls -la /usr/local/bin/protoc
   sudo cp -R include/google/protobuf/* /usr/local/include/
 else
   echo "Using cached directory."
